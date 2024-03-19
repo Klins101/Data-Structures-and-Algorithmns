41716af20b473a3abd600cf030757ad9562e6dba
Klinsmann Agyei <54152682+Klins101@users.noreply.github.com>
2024-03-19 18:42:32 +0300

    soln 2

--- twoNumberSum.py
+++ twoNumberSum.py
@@ -1,3 +1,7 @@
+
+# O(n^2) time | O(1) space
+
+
 def twoNumberSum(array, targetSum):
     for i in range(len(array) - 1):
         firstNum = array[i]
@@ -7,6 +11,24 @@ def twoNumberSum(array, targetSum):
                 return [firstNum, secondNumber]
     return []
 
+# O(nlog(n)) | O(1) space
+
+
+def twoNumberSum1(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        else:
+            right -= 1
+    return []
+
 
 if __name__ == "__main__":
-    print(twoNumberSum(array=[5, 3, 8, 11, -7, 2, -1], targetSum=10))
+    print(twoNumberSum(array=[5, 3, 8, 11, -7, 2, -1, 10, 7], targetSum=10))
+    print(twoNumberSum1(array=[5, 3, 8, 11, -7, 2, -1, 10, 7], targetSum=10))
