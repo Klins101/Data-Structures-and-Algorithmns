d9117d8469ab6e81d90133c9837b269618bb1f0c
Klinsmann Agyei <54152682+Klins101@users.noreply.github.com>
2024-03-19 21:21:39 +0300

    is Sequence valid

---
+++ validSequence.py
@@ -0,0 +1,13 @@
+def validSequence(array, sequence):
+    arrIdx = 0
+    seqIdx = 0
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+    return seqIdx == len(sequence)
+
+
+if __name__ == "__main__":
+    print(validSequence(array=[5, 3, 8, 11, -7,
+          2, -1, 10, 7], sequence=[3, 2, 10, 7]))
