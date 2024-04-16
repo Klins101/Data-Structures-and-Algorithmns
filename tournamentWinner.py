ec6d18b8ac17956d90ba8db260d8a84a1ef0f883
Klinsmann Agyei <54152682+Klins101@users.noreply.github.com>
2024-04-16 17:53:42 +0300

    Add test

--- tournamentWinner.py
+++ tournamentWinner.py
@@ -1,7 +1,7 @@
 def tournamentWinner(competitions, results):
     currentBestTeam = ""
     scores = {currentBestTeam: 0}
-    
+
     for idx, competition in enumerate(competitions):
         if results[idx] == 0:
             winner = competition[1]
@@ -17,3 +17,11 @@ def updateScores(scores, team, point):
     if team not in scores:
         scores[team] = 0
     scores[team] += 3
+
+
+
+
+if __name__ == "__main__":
+    competitions = [["Ghana", "Nigeria"], ["Nigeria", "Zimbabwe"], ["Ghana", "Zimbabwe"]]
+    results = [1, 0, 1]
+    print(tournamentWinner(competitions, results))
