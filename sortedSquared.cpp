858e5397d49d31a7b4c6cdff0103e83576c6970e
Klinsmann Agyei <54152682+Klins101@users.noreply.github.com>
2024-07-09 09:26:06 +0300

    ValidSequence

---
+++ sortedSquared.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> sortedSquared(vector<int> array){
+
+}
+
+int main(){
+    
+}
--- validSequence.cpp
+++ validSequence.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 using namespace std;
+#include <vector>
+
 
 bool validSequence(vector<int> array, vector<int> sequence){
+    int arrayIdx = 0;
+    int seqIdx = 0;
+    while (arrayIdx < array.size() && seqIdx < sequence.size()){
+        if (array[arrayIdx] == sequence[seqIdx]){
+            seqIdx += 1;
+            
+
+        }
+        arrayIdx += 1;
+    }
+    return seqIdx == sequence.size();
 
 }
 
 int main(){
-    
+    vector<int> array {3, 5, 1, 22, 25, 4, 8, -1, 16};
+    vector<int> sequence {1, 25, 8, -1 };
+    bool results = validSequence(array, sequence);
+    cout << results;
+
 }
