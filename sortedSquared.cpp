c2ea270ce149ae8701569c1082e2dd6791eb968d
Klinsmann Agyei <54152682+Klins101@users.noreply.github.com>
2024-07-09 10:01:01 +0300

    Sorted Squared

--- sortedSquared.cpp
+++ sortedSquared.cpp
@@ -1,11 +1,26 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+#include <algorithm>
+
 
 vector<int> sortedSquared(vector<int> array){
+    for (int i = 0; i < array.size(); i++){
+        array[i] = array[i] * array[i];
+    }
+    
+    sort(array.begin(), array.end());
+    return array;
 
 }
 
 int main(){
+    vector<int> array {1, -5, -3, 5, 6};
+    vector<int> results = sortedSquared(array);
     
+    for (auto x : results) {
+        cout << x << " ";
+    }
+
+
 }
